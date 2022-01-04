fdb49f0e75887cc456f5a18c192edd7445fa0f68
Conrado Buhrer <conrado@buhrer.net>
2022-01-04 14:27:39 -0300

    vid06 - list products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,49 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
 import Layout from '../components/Layout';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material';
+import data from '../utils/data';
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => {
+            return (
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Typography>${product.price}</Typography>
+                      <Button size="small" color="primary">
+                        Add to cart
+                      </Button>
+                    </CardActions>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
       </div>
     </Layout>
   );
