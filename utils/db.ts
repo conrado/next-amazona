73e87a972d2c1d9f6c3c72d7ac067429a8a5b0b2
Conrado Buhrer <conrado@buhrer.net>
2022-01-10 15:26:39 -0300

    vid12 - connect to mongodb

---
+++ utils/db.ts
@@ -0,0 +1,46 @@
+import mongoose, { MongooseOptions } from 'mongoose';
+
+interface Connection {
+  isConnected: number;
+}
+const connection: Connection = {
+  isConnected: 0,
+};
+
+const connect = async () => {
+  if (connection.isConnected) {
+    console.log('already connected!');
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log('use previous connection');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(
+    process.env.MONGODB_URI as string,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as MongooseOptions
+  );
+  console.log('new connection');
+  connection.isConnected = db.connections[0].readyState;
+};
+
+const disconnect = async () => {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+      connection.isConnected = 0;
+    } else {
+      console.log('not disconnected');
+    }
+  }
+};
+
+const db = { connect, disconnect };
+
+export default db;
